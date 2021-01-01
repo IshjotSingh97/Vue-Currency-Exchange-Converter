698695f4c0816f80b257682e391b843e00fdec39
Ishjot Singh <ishjotsinghahluwalia@gmail.com>
2021-01-01 14:46:29 +0530

    vue added

---
+++ index.js
@@ -0,0 +1,15 @@
+const myappcomponent = {
+    data(){
+        return {
+
+        }
+    },
+    methods : {
+
+    },
+    mounted(){
+
+    }
+}
+
+const myapp = Vue.createApp(myappcomponent).mount('#myapp')
