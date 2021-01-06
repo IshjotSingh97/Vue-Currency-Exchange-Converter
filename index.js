0e786c2b22b49005325a5898cafb9c05ea606f42
Ishjot Singh <ishjotsinghahluwalia@gmail.com>
2021-01-06 22:50:09 +0530

    test deploy

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-const API = `https://api.ratesapi.io/api/latest`
+const API = ""
+const host = 'api.frankfurter.app';
 const myappcomponent = {
     data(){
         return {
             currencyvalue : 1,
-            currencybase : null
+            currencybase : null,
+            currencies : [],
+            rates : []
         }
     },
     methods : {
-
+        async getExchangeRates(){
+            console.log("Request...hit")
+            console.log(`${this.currencyvalue}`)
+            console.log(`${this.currencybase}`)
+            let query = `https://api.exchangeratesapi.io/latest?base=${this.currencybase}`
+            console.log(query)
+            await axios.get(query)
+            .then((response)=>{
+                console.log(response.data)
+                this.rates = response.data.rates
+                console.log(this.rates)
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
+        },
+        async getCurriencies(){
+            await axios.get(`https://${host}/currencies`)
+            .then((response)=>{
+                this.currencies = response.data
+                console.log(this.currencies)
+                console.log(`https://${host}/currencies`)
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
+        }
     },
     mounted(){
-
+        this.getCurriencies()
     }
 }
 
