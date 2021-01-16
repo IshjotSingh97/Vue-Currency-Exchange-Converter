9120418aef6fc8086309101c2a0270fb54ad16d6
Ishjot Singh <ishjotsinghahluwalia@gmail.com>
2021-01-16 20:24:29 +0530

    fix bugs

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const API = ""
-const host = 'api.frankfurter.app';
 const myappcomponent = {
     data(){
         return {
@@ -12,27 +10,19 @@ const myappcomponent = {
     },
     methods : {
         async getExchangeRates(){
-            console.log("Request...hit")
-            console.log(`${this.currencyvalue}`)
-            console.log(`${this.currencybase}`)
             let query = `https://api.exchangeratesapi.io/latest?base=${this.currencyfrom}`
-            console.log(query)
             await axios.get(query)
             .then((response)=>{
-                console.log(response.data)
                 this.currencyto= response.data.rates
-                console.log(this.currencyto)
             })
             .catch((error)=>{
                 console.log(error)
             })
         },
         async getCurriencies(){
-            await axios.get(`https://${host}/currencies`)
+            await axios.get(`https://api.frankfurter.app/currencies`)
             .then((response)=>{
                 this.currencies = response.data
-                console.log(this.currencies)
-                console.log(`https://${host}/currencies`)
             })
             .catch((error)=>{
                 console.log(error)
